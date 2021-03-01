d72e8dc8a973269585a0cfea0225ec3060e25edb
saderdesigner <57695878+saderdesigner@users.noreply.github.com>
2021-03-01 17:29:37 +0700

    sorting

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ import ItemsList from "./components/items-list/items-list.component";
 import CustomInput from "./components/custom-input/custom-input.component";
 
 import { db, createTask } from "./firebase/firebase.utils";
+import firebase from "firebase";
 
 function App(props) {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState({
     title: "",
     completed: null,
+    timestamp: null,
   });
 
   useEffect(() => {
-    db.collection("todos").onSnapshot((snapShot) => {
-      setTasks(
-        snapShot.docs.map((doc) => {
-          return { _id: doc.id, ...doc.data() };
-        })
-      );
-    });
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapShot) => {
+        setTasks(
+          snapShot.docs.map((doc) => {
+            return { _id: doc.id, ...doc.data() };
+          })
+        );
+      });
   }, []);
 
   const handleSubmit = async (e) => {
@@ -30,10 +34,12 @@ function App(props) {
     newTask.title &&
       (await createTask({
         title: newTask.title,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       }));
     setNewTask({
       title: "",
       completed: null,
+      timestamp: null,
     });
   };
 
